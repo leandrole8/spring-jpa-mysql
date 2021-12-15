14270298cd56c1e15220251c8d6beed29b2a25c6
leandrole8 <leandro_le8@hotmail.com>
2021-12-15 13:41:26 -0300

    criado metodos para incluir, modificar e deletar contatos

--- src/main/java/com/leandroreis/springjpamysql/controller/ContactController.java
+++ src/main/java/com/leandroreis/springjpamysql/controller/ContactController.java
@@ -4,10 +4,7 @@ import com.leandroreis.springjpamysql.model.Contact;
 import com.leandroreis.springjpamysql.repository.ContactRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -26,10 +23,36 @@ public class ContactController {
         return repository.findAll();
     }
 
-    @GetMapping("/{id}")
+    @GetMapping(path = "/{id}")
     public ResponseEntity findById(@PathVariable long id){
         return repository.findById(id)
-                .map(record -> ResponseEntity.ok().body(record))
-                .orElse(ResponseEntity.notFound().build());
+                .map(record -> ResponseEntity.ok().body(record))//se encontrar retorna status 200 ok no corpo da resposta
+                .orElse(ResponseEntity.notFound().build());// caso nao encontre retorna status 404 nao encontrado.
+    }
+
+    @PostMapping
+    public Contact create(@RequestBody Contact contact){
+        return repository.save(contact);
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity update(@PathVariable("id") long id, @RequestBody Contact contact){
+        return repository.findById(id)
+                .map(record -> {
+                    record.setName(contact.getName());
+                    record.setEmail(contact.getEmail());
+                    record.setPhone(contact.getPhone());
+                    Contact updated = repository.save(record);
+                    return ResponseEntity.ok().body(updated);
+                }).orElse(ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public ResponseEntity<?> delete(@PathVariable long id){
+        return repository.findById(id)
+                .map(record -> {
+                    repository.deleteById(id);
+                    return ResponseEntity.ok().build();
+                }).orElse(ResponseEntity.notFound().build());
     }
 }
